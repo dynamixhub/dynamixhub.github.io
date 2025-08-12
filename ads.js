019151f627b47e5fad491544cfb7fc3817f4f7b3
dynamixhub <dynamixstudio7@gmail.com>
2025-08-12 23:37:46 +0530

    Delete ads.js

--- ads.js
+++
@@ -1,3 +0,0 @@
-// Replace with real AdSense code later
-document.getElementById('ad-top').innerHTML = '<!-- AdSense Slot Top -->';
-document.getElementById('ad-bottom').innerHTML = '<!-- AdSense Slot Bottom -->';
